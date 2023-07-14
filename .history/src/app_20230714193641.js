3878e9b35b5845732b53cdb19e26e055d0dee2bc
unknown <dovandaihoc@gmail.com>
2023-07-14 19:55:48 +0700

    section 2

---
+++ .history/src/app_20230714193641.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet);
+// init db
+
+// init routes
+
+// handling error
+
+module.exports = app;
